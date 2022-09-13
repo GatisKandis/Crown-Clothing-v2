cc0da756bf907e5ff4653ea520116b9b0ff19b29
DESKTOP-TTOK949\COREWARE-MATT <mat.lauigan@corewaretech.com.ph>
2022-09-13 15:53:43 +0800

    no message

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./routes/home/home.component";
+
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+
+    <Routes>
+      <Route path='/' index element={<Home />} />
+    </Routes>
+  );
+};
+
+export default App;
